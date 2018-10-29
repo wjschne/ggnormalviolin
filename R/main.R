d5b8778fd33b2ac29f85535501bbe055f3d168a7
W. Joel Schneider <w.joel.schneider@gmail.com>
2018-10-28 22:06:56 -0400

    many changes

--- R/main.R
+++ R/main.R
@@ -3,72 +3,189 @@
 #' @keywords internal
 #' @usage NULL
 #' @export
-StatNormViolin <- ggplot2::ggproto(
-  `_class` = "StatNormViolin",
+StatNormalViolin <- ggplot2::ggproto(
+  `_class` = "StatNormalViolin",
   `_inherit` = ggplot2::Stat,
+  required_aes = c("x","mu","sigma"),
+  default_aes = ggplot2::aes(width = 0.6,
+                    nsigma = 4),
+  # setup_params = function(data, params) {
+  #   str(params)
+  #
+  #   if (is.null(params$width))
+  #     params$width <- 0.6
+  #
+  #   if (is.null(params$nsigma))
+  #     params$nsigma <- 4
+  #
+  #   params
+  # },
+  setup_data = function(data, params) {
+    if (is.null(data$width))
+      data$width = params$width
+    if (is.null(data$nsigma))
+      data$nsigma = params$nsigma
+
+    data$group <- 1:nrow(data)
+
+    data
+  },
   compute_group = function(
+    self,
     data,
     scales,
-    nsigma = 4,
-    width = 0.4,
-    p_tail = NULL,
-    upper_bound = data$mu + nsigma * data$sigma,
-    lower_bound = data$mu - nsigma * data$sigma) {
-    signx <- c(rep(1, length(seq(0, nsigma,0.01))),
-               rep(-1, length(seq(nsigma, -1 * nsigma,-0.01))),
-               rep(1, length(seq(-1 * nsigma, 0,0.01)))
-               )
-    y <- c(seq(0, nsigma,0.01),
-           seq(nsigma, -1 * nsigma,-0.01),
-           seq(-1 * nsigma, 0,0.01)) *  data$sigma + data$mu
-    xpos <- signx * width * dnorm(y, data$mu,  data$sigma) / dnorm( data$mu,  data$mu,  data$sigma) +  data$x
-    d <- data.frame(x = xpos, y = y)
-    d <- dplyr::filter(d, y <= upper_bound & y >= lower_bound)
-    if (!is.null(p_tail)) {
-      d[(d$y <= -1 * qnorm(p_tail / 2) * data$sigma + data$mu) &
-        (d$y >= qnorm(p_tail / 2) * data$sigma + data$mu),"y"] <- NA
-    }
+    lower_limit = NA,
+    upper_limit = NA,
+    width = 0.6,
+    nsigma = 4
+  ) {
+    # The y values start at right side at 0, go up to the positive tail,
+    # reverse to left side, go to the negative tail,
+    # reverse again to right side, and then return to 0.
+    # This seemingly odd sequence was needed to make
+    # polygons clipped with p_tail appear correctly.
+
+    y <- c(seq(0, data$nsigma, by = 0.01),
+           seq(data$nsigma,-1 * data$nsigma, by = -0.01),
+           seq(-1 * data$nsigma, 0, by = 0.01)) *  data$sigma + data$mu
+
+    # Which direction the violin should deviate?
+    signx <- c(rep(1, length(seq(0, data$nsigma,0.01))),
+               rep(-1, length(seq(data$nsigma, -1 * data$nsigma,-0.01))),
+               rep(1, length(seq(-1 * data$nsigma, 0,0.01)))
+    )
+
+    # Set x position of violin
+    xpos <- 0.5 * signx * data$width * dnorm(y, data$mu,  data$sigma) / dnorm( data$mu,  data$mu,  data$sigma) +  data$x
+
+    # Make data.frame
+    d <- data.frame(x = xpos,
+                    y = y,
+                    group = data$group,
+                    mu = data$mu,
+                    sigma = data$sigma)
+
+    if (!is.na(upper_limit)) d <- dplyr::filter(d, y <= upper_limit)
+    if (!is.na(lower_limit)) d <- dplyr::filter(d, y >= lower_limit)
     d
-  },
-  required_aes = c("x","mu","sigma")
+  }
 )
-# GeomNormViolin <- ggplot2::ggproto(
-#   `_class` = "GeomNormViolin",
-#   `_inherit` = ggplot2::Geom,
-#   default_aes = ggplot2::aes(
-#     width = 0.4,
-#     linetype = "solid",
-#     fontsize = 5,
-#     shape = 16,
-#     colour = NA,
-#     size = .1,
-#     fill = "black",
-#     alpha = 1,
-#     stroke = 0.1,
-#     linewidth = .1,
-#     weight = 1,
-#     x = NULL,
-#     y = NULL,
-#     conds = NULL,
-#     nsigma = 4
-#   ),
-#   setup_data = function(
-#     data,
-#     params) {
-#     y <- seq(-1 * params$nsigma, params$nsigma,0.01) *  data$sigma + data$mu
-#     xpos <- params$width * dnorm(y, data$mu,  data$sigma) / dnorm( data$mu,  data$mu,  data$sigma)
-#     xpos <- c(xpos,-rev(xpos)) +  data$x
-#     y <- c(y, rev(y))
-#     data.frame(x = xpos, y = y)
-#   },
-#   draw_group = ggplot2::GeomPolygon$draw_group,
-#   required_aes = c("x","mu","sigma")
-# )
+
+#' GeomNormalViolin
+#'
+#' @format NULL
+#' @keywords internal
+#' @usage NULL
+#' @export
+GeomNormalViolin <- ggplot2::ggproto(
+  `_class` = "GeomNormalViolin",
+  `_inherit` = ggplot2::Geom,
+  required_aes = c("x", "mu", "sigma"),
+  default_aes = ggplot2::aes(
+    shape = 16,
+    colour = NA,
+    size = 0.5,
+    fill = "gray70",
+    alpha = 1,
+    stroke = 0.5,
+    tail_fill = "black",
+    tail_alpha = 1,
+    p_tail = 0,
+    p_lower_tail = 0,
+    p_upper_tail = 0
+  ),
+  draw_key = ggplot2::draw_key_polygon,
+  draw_panel = function(
+    self,
+    data,
+    panel_scales,
+    coord) {
+
+    # Parameters for each violin
+    d_param <- data %>%
+      dplyr::group_by(group) %>%
+      dplyr::summarise_all(.funs = list(dplyr::first))
+
+    # Violin points transformed for grid coordinates
+    dpoints <- coord$transform(data, panel_scales)
+
+    # Violin polygons
+    gbody <- grid::polygonGrob(
+      x = dpoints$x,
+      y = dpoints$y,
+      id = dpoints$group,
+      gp = grid::gpar(col = d_param$colour,
+                      fill = d_param$fill,
+                      alpha = d_param$alpha)
+    )
+
+    # Filter data for upper tail
+    data_uppertail <- data %>%
+      dplyr::mutate(UB = qnorm(1 - p_upper_tail) * sigma + mu) %>%
+      dplyr::filter(y >= UB)
+
+    if (nrow(data_uppertail) > 0) {
+      # Transform upper tail data for grid coordinates
+      duppertail <- coord$transform(data_uppertail, panel_scales)
+
+      # Make upper tail polygon
+      g_upper_tail <- grid::polygonGrob(
+        x = duppertail$x,
+        y = duppertail$y,
+        id = duppertail$group,
+        gp = grid::gpar(
+          col = d_param$colour,
+          fill = d_param$tail_fill,
+          alpha = d_param$tail_alpha
+        )
+      )
+    } else {
+      g_upper_tail <- grid::nullGrob()
+    }
+
+    # Filter data for lower tail
+    data_lowertail <- data %>%
+      dplyr::mutate(LB = qnorm(p_lower_tail) * sigma + mu) %>%
+      dplyr::filter(y <= LB)
+
+    if (nrow(data_lowertail) > 0) {
+      # Transform lower tail data for grid coordinates
+      dlowertail <- coord$transform(data_lowertail, panel_scales)
+
+      # Make lower tail polygon
+      g_lower_tail <- grid::polygonGrob(
+        x = dlowertail$x,
+        y = dlowertail$y,
+        id = dlowertail$group,
+        gp = grid::gpar(
+          col = d_param$colour,
+          fill = d_param$tail_fill,
+          alpha = d_param$tail_alpha
+        )
+      )
+    } else {
+      g_lower_tail <- grid::nullGrob()
+    }
+    grid::gTree(children = grid::gList(gbody, g_upper_tail, g_lower_tail))
+  }
+)
+
+
+
 
 #' Creates normal violins with specified means and standard deviations
 #'
 #' @inheritParams  ggplot2::layer
 #' @inheritParams ggplot2::geom_polygon
+#' @param nsigma The number of standard deviations each violin should extend
+#' @param p_tail The 2-tailed proportion that should be highlighted. Can be overridden with p_lower_tail and/or p_upper_tail
+#' @param p_upper_tail The proportion of the distribution that should be highlighted in the upper tail. Defaults to half of `p_tail`.
+#' @param p_lower_tail The proportion of the distribution that should be highlighted in the lower tail. Defaults to half of `p_tail`.
+#' @param tail_fill fill color for tails
+#' @param tail_alpha alpha value for tails
+#' @param upper_limit upper limit for polygons. Needed in case setting limits in scale_y_continuous or ylim distorts the polygons.
+#' @param lower_limit lower limit for polygons. Needed in case setting limits in scale_y_continuous or ylim distorts the polygons.
+#' @param width Width of normal violin
 #' @section Aesthetics:
 #' \code{geom_normviolin} understands the following aesthetics (required aesthetics are in bold):
 #' \itemize{
@@ -81,11 +198,13 @@ StatNormViolin <- ggplot2::ggproto(
 #'   \item group
 #'   \item linetype
 #'   \item size
+#'   \item width
+#'   \item nsigma (number of standard deviations to which the violins extend)
 #' }
-#' @export
 #' @examples
 #' library(ggplot2)
-#' library(ggnormviolin)
+#' library(ggnormalviolin)
+#'
 #' d <- data.frame(
 #'   Distribution = c("A", "B"),
 #'   Distribution_mean = c(80, 90),
@@ -93,26 +212,86 @@ StatNormViolin <- ggplot2::ggproto(
 #' )
 #'
 #' ggplot(data = d, aes(x = Distribution)) +
-#'   geom_normviolin(aes(mu = Distribution_mean,
+#'   geom_normalviolin(aes(mu = Distribution_mean,
 #'                       sigma = Distribution_sd))
-geom_normviolin <- function(
+#' @export
+geom_normalviolin <- function(
   mapping = NULL,
   data = NULL,
-  geom = "polygon",
-  position = "identity",
+  nsigma = 4,
+  p_tail = 0,
+  p_lower_tail = p_tail / 2,
+  p_upper_tail = p_tail / 2,
+  tail_fill = "black",
+  tail_alpha = 0.4,
+  upper_limit = NA,
+  lower_limit = NA,
   na.rm = FALSE,
   show.legend = NA,
   inherit.aes = TRUE,
-  ...) {
+  width = 0.6,
+  ...
+) {
+
   ggplot2::layer(
-    stat = StatNormViolin,
     data = data,
     mapping = mapping,
-    geom = "polygon",
-    position = position,
+    stat = StatNormalViolin,
+    geom = GeomNormalViolin,
+    position = "identity",
     show.legend = show.legend,
     inherit.aes = inherit.aes,
-    params = list(na.rm = na.rm, ...)
+     params = list(
+      na.rm = na.rm,
+      nsigma = nsigma,
+      p_tail = p_tail,
+      p_lower_tail = p_lower_tail,
+      p_upper_tail = p_upper_tail,
+      tail_fill = tail_fill,
+      tail_alpha = tail_alpha,
+      upper_limit = upper_limit,
+      lower_limit = lower_limit,
+      width = width,
+      ...
+    )
   )
-
 }
+
+# # Make data
+# d <- tibble::tibble(
+#   Distribution = c("A","B","C","D"),
+#   Distribution_mean = c(80, 90, 110, 130),
+#   Distribution_sd = c(15, 10, 20, 5),
+#   width = 0.04 * Distribution_sd,
+#   type = c(1,1,2,2)
+# )
+#
+#
+#
+# # Make Plot
+# ggplot(data = d, aes(x = Distribution)) +
+#   geom_normalviolin(aes(mu = Distribution_mean,
+#                         sigma = Distribution_sd,
+#                         fill = Distribution),
+#                     p_tail = 0.05)
+#
+# ggplot(data = d, aes(x = Distribution)) +
+#   geom_normalviolin(aes(mu = Distribution_mean,
+#                         sigma = Distribution_sd,
+#                         fill = Distribution),
+#                     p_tail = 0.05,
+#                     width = 2,
+#                     nsigma = 2)
+#
+# ggplot(data = d, aes(x = Distribution)) +
+#   geom_normalviolin(aes(mu = Distribution_mean,
+#                         sigma = Distribution_sd,
+#                         fill = Distribution,
+#                         width = Distribution_sd / 20,
+#                         nsigma = 1:4),
+#                     p_tail = 0.05)
+#
+#
+# StatNormalViolin$parameters(TRUE)
+# GeomNormalViolin$parameters(TRUE)
+# StatNormalViolin$default_aes
--- README.md
+++ README.md
@@ -1,25 +1,29 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# ggnormviolin <img src="man/figures/logo.png" align="right" height=140/>
+# ggnormalviolin <img src="man/figures/logo.png" align="right" height=140/>
 
-The ggnormviolin package uses ggplot2 to create normal violin plots with
-specified means and standard deviations.
+[![CRAN
+status](https://www.r-pkg.org/badges/version/ggnormalviolin)](https://cran.r-project.org/package=ggnormalviolin)
+[![lifecycle](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://www.tidyverse.org/lifecycle/#experimental)
+
+The ggnormalviolin package uses ggplot2 to create normal violin plots
+with specified means and standard deviations.
 
 ## Installation
 
-To install the development version of ggnormviolin, you need to check if
-devtools is installed. If not, run this:
+To install the development version of ggnormalviolin, you need to check
+if devtools is installed. If not, run this:
 
 ``` r
 install.packages("devtools")
 ```
 
 Once you are sure you have devtools installed, you can install the
-development version of ggnormviolin from GitHub by running this code:
+development version of ggnormalviolin from GitHub by running this code:
 
 ``` r
-devtools::install_github("wjschne/ggnormviolin")
+devtools::install_github("wjschne/ggnormalviolin")
 ```
 
 ## Example
@@ -29,42 +33,91 @@ means and standard deviations. They can be plotted like so:
 
 ``` r
 library(ggplot2)
-library(ggnormviolin)
+library(ggnormalviolin)
 
 # Make data
 d <- data.frame(
-  Distribution = c("A", "B", "C", "D"),
-  Distribution_mean = c(80, 90, 110, 130),
-  Distribution_sd = c(15, 10, 20, 5)
+  dist = c("A", "B", "C", "D"),
+  dist_mean = c(80, 90, 110, 130),
+  dist_sd = c(15, 10, 20, 5)
 )
 
+
+
 # Make Plot
-ggplot(data = d, aes(x = Distribution)) +
-  geom_normviolin(aes(mu = Distribution_mean, 
-                      sigma = Distribution_sd), 
-                  width = 0.25,
-                  fill = "dodgerblue") 
+ggplot(data = d, aes(x = dist)) +
+  geom_normalviolin(aes(mu = dist_mean, 
+                      sigma = dist_sd,
+                      fill = dist))
 ```
 
 <img src="man/figures/README-example-1.svg" width="100%" />
 
-Suppose we want to highlight the two tails of the distributions. We can
-add a second layer with just the tails represending the outermost 2.5%
-of the distributions. Set the `p_tails` to specify the total aread of
-the tails. Thus, if `p_tails` = 0.05, each tail will represent the
-outermost 2.5% of the distributions (i.e, 0.05 = 2 \&mult; 0.025).
+Suppose we want to highlight the two tails of the distributions. Set the
+`p_tails` to specify the total area of the tails. Thus, if `p_tail` =
+0.05, each tail will represent the outermost 2.5% of the distributions
+(i.e, 0.05 = 2 \&mult; 0.025).
 
 ``` r
-ggplot(data = d, aes(x = Distribution)) +
-  geom_normviolin(aes(mu = Distribution_mean, 
-                      sigma = Distribution_sd), 
-                  width = 0.25, 
-                  fill = "dodgerblue") +
-  geom_normviolin(aes(mu = Distribution_mean, 
-                      sigma = Distribution_sd), 
-                  width = 0.25,
-                  fill = "dodgerblue4",
+ggplot(data = d, aes(x = dist)) +
+  geom_normalviolin(aes(mu = dist_mean, 
+                      sigma = dist_sd,
+                      fill = dist), 
+                  fill  = "dodgerblue",
                   p_tail = 0.05)
 ```
 
 <img src="man/figures/README-example2-1.svg" width="100%" />
+
+Suppose we want to highly only the upper tails. Set \`p\_upper\_tail to
+the proportion desired.
+
+``` r
+ggplot(data = d, aes(x = dist)) +
+  geom_normalviolin(aes(mu = dist_mean, 
+                      sigma = dist_sd,
+                      fill = dist), 
+                  p_upper_tail = 0.05)
+```
+
+<img src="man/figures/README-unnamed-chunk-3-1.svg" width="100%" />
+
+Analogously, we can highlight only the lower tails
+
+``` r
+ggplot(data = d, aes(x = dist)) +
+  geom_normalviolin(aes(mu = dist_mean, 
+                      sigma = dist_sd), 
+                  p_lower_tail = 0.05)
+```
+
+<img src="man/figures/README-unnamed-chunk-4-1.svg" width="100%" />
+
+We can set with width of each violin with `width`.
+
+``` r
+ggplot(data = d, aes(x = dist)) +
+  geom_normalviolin(aes(mu = dist_mean, 
+                      sigma = dist_sd), 
+                  width = 0.3)
+```
+
+<img src="man/figures/README-unnamed-chunk-5-1.svg" width="100%" />
+
+If you want the shape of the distribution to remain constant, map the
+width parameter to a multiple of the standard deviation.
+
+``` r
+ggplot(data = d, aes(x = dist)) +
+  geom_normalviolin(aes(mu = dist_mean, 
+                      sigma = dist_sd,
+                      width = dist_sd * 0.05 ))
+```
+
+<img src="man/figures/README-unnamed-chunk-6-1.svg" width="100%" />
+
+## Code of Conduct
+
+Please note that the ‘ggnormalviolin project’ is released with a
+[Contributor Code of Conduct](CODE_OF_CONDUCT.md). By contributing to
+this project, you agree to abide by its terms.
